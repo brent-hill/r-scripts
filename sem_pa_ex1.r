ffb9543ac5f069f87c3cfad996fc596eb449cf2e
Brent Hill <brent.hill@ndsu.edu>
2025-02-19 15:30:12 -0600

    SEM-PA examples updated

--- sem_pa_ex1.r
+++ sem_pa_ex1.r
@@ -2,23 +2,47 @@
 # Data file: student_profiles.rda
 # - Data frame: profile
 
-# Model and estimation
+# Example highlights:
+# - This model has a very good fit, so no model modification (respecification) is needed.
+# - Estimating/testing indirect effects is also demonstrated.
+
+# Initial model
 model <- '
 gpa ~ am + iq
 am ~ ses
 '
 sol <- sem(model, profile)
 
-# Overall results
-summary(sol, standardized=TRUE, fit.measures=TRUE, modindices=TRUE)
+# - Parameter estimates and significance tests
+summary(sol, standardized=TRUE)
 parameterEstimates(sol, standardized=TRUE)
 
-# Global fit
+# - Global fit
 fitMeasures(sol)   # All global test
 fitMeasures(sol, c("chisq","df","pvalue"))   # Request specific test statistics
 fitMeasures(sol, c("cfi","rmsea","srmr"))
 
-# Diagnostics for model modification
-modindices(sol)
-resid(sol, type="standardized")
-resid(sol, type="normalized")
+# - Diagnostics for model modification
+# - - Modification indices (MI)
+modificationIndices(sol)
+# - - Covariance residuals
+resid(sol, type="standardized")   # Computes SCRs
+resid(sol, type="normalized")     # Computes NCRs
+
+
+
+# Estimating/testing indirect effects
+# - Sobel method
+model_ie <- '
+gpa ~ B21*am + G22*iq
+am ~ G11*ses
+ie_gpa.ses := B21*G11
+'
+sol_ie <- sem(model_ie, profile)
+summary(sol_ie, standardized=TRUE)
+parameterEstimates(sol_ie, standardized=TRUE)
+
+# - Bootstrapping method
+sol_ie_bs <- sem(model_ie, profile, se="bootstrap", bootstrap=1000)
+summary(sol_ie_bs, standardized=TRUE)
+parameterEstimates(sol_ie_bs, standardized=TRUE)
--- sem_pa_ex2.r
+++ sem_pa_ex2.r
@@ -3,20 +3,20 @@
 # - Matrix: Cov
 
 # Example highlights:
-# - Some model modification (respecification) needed
-# - Input is covariance matrix
+# - The fit of the initial model is not good, so some model modification (respecification) is needed.
+# - Input data is a covariance matrix.
 # - Data taken from a published journal article (Ingram et al., 2001)
 # - Data provided in article as correlation matrix and vector of SDs; this is also an example of how to (1) copy that data into R and (2) convert those into a covariance matrix.
 
-# Model and estimation
+# Initial model
 model <- '
 behavior ~ intent
 intent ~ attitude + subnorm + perbhvcon
 '
 sol <- sem(model, sample.cov=Cov, sample.nobs=60)   # N=60 (reported in article)
 
-# Overall results
-summary(sol, standardized=TRUE, fit.measures=TRUE, modindices=TRUE)
+# Parameter estimates and significance tests
+summary(sol, standardized=TRUE)
 parameterEstimates(sol, standardized=TRUE)
 
 # Global fit
@@ -24,8 +24,8 @@ fitMeasures(sol, c("chisq","df","pvalue"))
 fitMeasures(sol, c("cfi","tli","rmsea","srmr"))
 
 # Diagnostics for model modification
-modindices(sol)                    # Shows all MIs
-modindices(sol, minimum.value=5)   # Show only MIs ≥ 5
+modificationIndices(sol)                    # Shows all MIs
+modificationIndices(sol, minimum.value=5)   # Show only MIs ≥ 5
 resid(sol, type="standardized")
 resid(sol, type="normalized")
 
@@ -37,4 +37,4 @@ model2 <- '
 behavior ~ intent + perbhvcon
 intent ~ attitude + subnorm + perbhvcon
 '
-# Run this new model and assess global fit.
+# Run this new model and assess fit.
